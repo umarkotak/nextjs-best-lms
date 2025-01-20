120699cff6cacc4eca06ece38cd053e1e8c09358
umar.ramadhana <umar.ramadhana@everywhere.id>
2025-01-20 21:26:47 +0700

    base layout

---
+++ components/layouts/AdminLayout.js
@@ -0,0 +1,117 @@
+import * as React from "react"
+import { Calendar, GraduationCap, Home, Inbox, Search, Settings, Slack, SlackIcon } from "lucide-react"
+import {
+  SidebarProvider,
+  SidebarTrigger,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarInset,
+} from "@/components/ui/sidebar"
+import { Separator } from "@radix-ui/react-dropdown-menu"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "../ui/breadcrumb"
+import { SidebarUser } from "./SidebarUser"
+import { ChangeThemeButton } from "../utils/ChangeThemeButton"
+
+const items = [
+  { title: "Home", url: "#", icon: Home },
+  { title: "Inbox", url: "#", icon: Inbox },
+  { title: "Calendar", url: "#", icon: Calendar },
+  { title: "Search", url: "#", icon: Search },
+  { title: "Settings", url: "#", icon: Settings },
+]
+
+export function AdminLayout({ children }) {
+  return (
+    <>
+      <SidebarProvider>
+        <Sidebar collapsible="icon">
+          <SidebarHeader>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton
+                  size="lg"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                    <GraduationCap className="size-4" />
+                  </div>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      Best LMS
+                    </span>
+                    <span className="truncate text-xs">help you learn</span>
+                  </div>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarHeader>
+
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupLabel>Application</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <a href={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+
+          <SidebarFooter>
+            <SidebarUser user={{
+              avatar: "https://placehold.co/300",
+              name: "m umar r",
+              email: "codingmase@gmail.com",
+            }} />
+          </SidebarFooter>
+        </Sidebar>
+
+        <SidebarInset>
+          <header className="sticky top-0 flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 backdrop-blur-md  bg-opacity-80">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">
+                      Building Your Application
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+            <div className="flex justify-end items-center gap-2 px-4">
+              <ChangeThemeButton />
+            </div>
+          </header>
+
+          <div className="p-4 pt-0">
+            {children}
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,45 @@
 import "@/styles/globals.css";
 
+import { ThemeProvider } from "@/components/layouts/ThemeProvider";
+import { CookiesProvider } from "react-cookie";
+import Head from "next/head";
+import { usePathname } from "next/navigation";
+import { DefaultLayout } from "@/components/layouts/DefaultLayout";
+import { AdminLayout } from "@/components/layouts/AdminLayout";
+import { Geist, Roboto } from 'next/font/google'
+
+const roboto = Geist({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const pathName = usePathname()
+
+  return (
+    <>
+      <CookiesProvider defaultSetOptions={{ path: '/' }}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+        >
+          <Head>
+            <title>Best LMS</title>
+          </Head>
+
+          <main className={roboto.className}>
+            {/* <DefaultLayout>
+              <Component {...pageProps} />
+            </DefaultLayout> */}
+
+            <AdminLayout>
+              <Component {...pageProps} />
+            </AdminLayout>
+          </main>
+
+        </ThemeProvider>
+      </CookiesProvider>
+    </>
+  );
 }
